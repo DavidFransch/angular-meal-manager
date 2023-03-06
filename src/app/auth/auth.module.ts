18e65026ebeebd028064c7d494646b86021c4e8f
DavidFransch <djsfdavid@gmail.com>
2023-03-06 16:03:30 +0200

    feat: create modules auth and core

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {AuthComponent} from "./auth.component";
+import {SharedModule} from "../shared/shared.module";
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    RouterModule.forChild([{ path: 'auth', component: AuthComponent }]),
+    SharedModule,
+    FormsModule
+  ]
+})
+export class AuthModule{}
