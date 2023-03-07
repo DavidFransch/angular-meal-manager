b5b901c088cf9758ad2e5068a54bc48978877aca
DavidFransch <djsfdavid@gmail.com>
2023-03-07 09:50:45 +0200

    feat: add lazy loading and preloading

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,7 +7,7 @@ import {RouterModule} from "@angular/router";
 @NgModule({
   declarations: [AuthComponent],
   imports: [
-    RouterModule.forChild([{ path: 'auth', component: AuthComponent }]),
+    RouterModule.forChild([{ path: '', component: AuthComponent }]),
     SharedModule,
     FormsModule
   ]
